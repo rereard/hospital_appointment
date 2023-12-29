1d284f9781f77ea582b97be1122206e620d90ba7
rereard <rereardany60@gmail.com>
2023-12-29 23:11:54 +0700

    ui login and pasien

--- daftar_baru/index.php
+++ daftar_baru/index.php
@@ -4,28 +4,28 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>RS Capstone</title>
+  <title>Pendaftaran - RS Capstone</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.css">
+  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="../plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -45,7 +45,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="" class="brand-link">
+      <a href="../" class="brand-link">
         <img src="https://img.freepik.com/free-vector/hospital-building-concept-illustration_114360-8440.jpg?w=740&t=st=1703855634~exp=1703856234~hmac=5deb2bad6c59edea43525c543223f70a5f85327cd1a58e39054c03b365267b9d" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <!-- <i class="nav-icon fas fa-user-tie brand-image"></i> -->
         <span class="brand-text font-weight-light">Capstone Hospital</span>
@@ -57,7 +57,7 @@
         <nav class="mt-2">
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
-              <a href="daftar_baru" class="nav-link">
+              <a href="../daftar_baru/" class="nav-link">
                 <i class="nav-icon fas fa-user-doctor"></i>
                 <p>
                   Pendaftaran Baru
@@ -65,7 +65,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a href="daftar_poli" class="nav-link">
+              <a href="../daftar_poli/" class="nav-link">
                 <i class="nav-icon fas fa-house-chimney-medical"></i>
                 <p>
                   Pendaftaran Poli
@@ -85,7 +85,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1 class="m-0">Home</h1>
+              <h1 class="m-0">Pendaftaran Pasien Baru</h1>
             </div><!-- /.col -->
           </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -94,24 +94,35 @@
       <!-- Main content -->
       <section class="content">
         <div class="container-fluid">
-          <!-- Small boxes (Stat box) -->
-          <div class="row">
-            <div class="col-lg">
-              <div class="card">
-                <div class="card-header">
-                  <h5 class="m-0">Welcome!</h5>
+          <div class="card">
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <label for="addNamaPasien">Nama Lengkap</label>
+                  <input type="text" class="form-control" id="addNamaPasien" placeholder="Nama Lengkap" />
                 </div>
-                <div class="card-body">
-                  <p class="card-text">Selamat datang di Rumah Sakit Capstone. Klik tombol di bawah untuk mendaftar poli.</p>
-                  <a href="daftar_poli" class="btn btn-primary">Pendaftaran Poli</a>
-                  <br></br>
-                  <p class="card-text">Bila belum pernah berobat di RS Capstone, silahkan klik</p>
-                  <a href="daftar_baru" class="btn btn-primary">Pendaftaran Baru</a>
+                <div class="form-group">
+                  <label for="addAlamatPasien">Alamat Tempat Tinggal</label>
+                  <input type="text" class="form-control" id="addAlamatPasien" placeholder="Alamat Tempat Tinggal" />
                 </div>
-              </div><!-- /.card -->
+                <div class="form-group">
+                  <label for="addNoKTPPasien">Nomor KTP</label>
+                  <input type="text" class="form-control" id="addNoKTPPasien" placeholder="Nomor KTP" />
+                </div>
+                <div class="form-group">
+                  <label for="addNoHPPasien">Nomor HP</label>
+                  <input type="text" class="form-control" id="addNoHPPasien" placeholder="Nomor HP" />
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <button type="submit" class="btn btn-primary">
+                      Daftar
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-          <!-- /.row -->
         </div><!-- /.container-fluid -->
       </section>
       <!-- /.content -->
@@ -120,39 +131,39 @@
   <!-- ./wrapper -->
 
   <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
+  <script src="../plugins/jquery/jquery.min.js"></script>
   <!-- jQuery UI 1.11.4 -->
-  <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+  <script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
   <script>
     $.widget.bridge('uibutton', $.ui.button)
   </script>
   <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- ChartJS -->
-  <script src="plugins/chart.js/Chart.min.js"></script>
+  <script src="../plugins/chart.js/Chart.min.js"></script>
   <!-- Sparkline -->
-  <script src="plugins/sparklines/sparkline.js"></script>
+  <script src="../plugins/sparklines/sparkline.js"></script>
   <!-- JQVMap -->
-  <script src="plugins/jqvmap/jquery.vmap.min.js"></script>
-  <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+  <script src="../plugins/jqvmap/jquery.vmap.min.js"></script>
+  <script src="../plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
   <!-- jQuery Knob Chart -->
-  <script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+  <script src="../plugins/jquery-knob/jquery.knob.min.js"></script>
   <!-- daterangepicker -->
-  <script src="plugins/moment/moment.min.js"></script>
-  <script src="plugins/daterangepicker/daterangepicker.js"></script>
+  <script src="../plugins/moment/moment.min.js"></script>
+  <script src="../plugins/daterangepicker/daterangepicker.js"></script>
   <!-- Tempusdominus Bootstrap 4 -->
-  <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+  <script src="../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
   <!-- Summernote -->
-  <script src="plugins/summernote/summernote-bs4.min.js"></script>
+  <script src="../plugins/summernote/summernote-bs4.min.js"></script>
   <!-- overlayScrollbars -->
-  <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
   <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.js"></script>
+  <script src="../dist/js/adminlte.js"></script>
   <!-- AdminLTE for demo purposes -->
-  <script src="dist/js/demo.js"></script>
+  <script src="../dist/js/demo.js"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-  <script src="dist/js/pages/dashboard.js"></script>
+  <script src="../dist/js/pages/dashboard.js"></script>
 </body>
 
 </html>
